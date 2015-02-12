845ddc1182f6beb3a8128a6a5a09a7667192d5f8
sanic888 <sanic888@gmail.com>
2015-02-11 22:48:18 -0800

    part of lesson 11

--- microscope/collections/comments.js
+++ microscope/collections/comments.js
@@ -11,14 +11,19 @@ Meteor.methods({
   			throw new Meteor.Error(422, 'Please write some content');
 		if (!post)
 	  		throw new Meteor.Error(422, 'You must comment on a post');
-		comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
-	  		userId: user._id,
-		  	author: user.username,
-		  	submitted: new Date().getTime()
-		});
 
-		Posts.update(comment.postId, {$inc: {commentsCount: 1}});
-		
-		return Comments.insert(comment);
+	  	comment = _.extend(commentAttributes,
+							{
+								userId: user._id,
+								author: user.userName,
+								submitted: new Date()
+							});
+
+		Post.update(comment.postId, {$inc: {commentsCount: 1}});
+
+	    comment._id = Comments.insert(comment);
+
+	    createCommentNotification(comment);
+	    return comment._id;
 	}
 });
