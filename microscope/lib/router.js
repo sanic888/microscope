6d8b5f93068b591f661f1d90baa0479ad1a9d457
sanic888 <sanic888@gmail.com>
2015-03-23 11:00:19 -0700

    completed chapter 12

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,26 +1,86 @@
+var postsLimit;
+PostsListController = RouteController.extend({
+  template: 'postsList',
+  _increment: 5,
+  _limit: function() {
+  	postsLimit = parseInt(this.params.postsLimit) || this._increment;
+    return postsLimit;
+  },
+  _findOptions: function() {
+    return {sort: {submitted: -1}, limit: this._limit()};
+  },
+  // waitOn: function() {
+  //   return Meteor.subscribe('posts', this._findOptions());
+  // },
+  // data: function() {
+  //   return {posts: Posts.find({}, this._findOptions())};
+  // },
+  onBeforeAction: function() {
+    this.postsSub = Meteor.subscribe('posts', this._findOptions());
+    this.next();
+  },
+  posts: function() {
+    return Posts.find({}, this._findOptions());
+  },
+  data: function() {
+    var hasMore = this.posts().fetch().length === this._limit();
+    var nextPath = this.route.path({postsLimit: this._limit() + this._increment});
+    return {
+      posts: this.posts(),
+      nextPath: hasMore ? nextPath : null
+    };
+  }
+});
+
 Router.configure({
 	layoutTemplate: 'layout',
 	loadingTemplate: 'loading',
 	notFoundTemplate: "notFound",
 	waitOn: function(){
-		return [Meteor.subscribe('posts'), Meteor.subscribe('notifications')];
+		return [Meteor.subscribe('notifications')];
 	}
 });
 
 Router.map(function(){
-	this.route('postsList', {path: '/'});
 	this.route('postPage', {
 		path: '/posts/:_id', 
 		data: function(){
-			return Posts.findOne(this.params._id)
+			return Posts.findOne(this.params._id);
 		}, 
 		waitOn: function(){
-			return Meteor.subscribe('comments', this.params._id);
+			return [
+				Meteor.subscribe('singlePost', this.params._id),
+				Meteor.subscribe('comments', this.params._id)
+			];
 		}
 	});
-	this.route('postSubmit', {path: '/submit'});
-	this.route('postEdit', {path: '/posts/:_id/edit', data: function(){return Posts.findOne(this.params._id)}});
+	this.route('postSubmit', {
+		path: '/submit', 
+		disableProgress: true
+	});
+	this.route('postEdit', {
+		path: '/posts/:_id/edit', 
+		waitOn: function(){
+			return Meteor.subscribe('singlePost', this.params._id);
+		},
+		data: function(){return Posts.findOne(this.params._id)}
+	});
+	this.route('postsList', {
+		path: '/:postsLimit?',
+/*		waitOn: function() {
+	      postsLimit = parseInt(this.params.postsLimit) || 5;
+	      return Meteor.subscribe('posts', {sort: {submitted: -1}, limit: postsLimit});
+	    },
+	    data: function() {
+	      //var limit = parseInt(this.params.postsLimit) || 5;
+	      return {
+	        posts: Posts.find()
+	      };
+	    },*/
+	    controller: PostsListController
+	});
 });
+
 
 var requireLogin = function(){
 	if (!Meteor.user()){
